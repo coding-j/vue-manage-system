e6dbd5746e323500b5db5ca49759a8f9515814ca
coding-j <401236678@qq.com>
2019-04-04 20:02:21 +0800

    Dashboard

--- src/components/page/Dashboard.vue
+++ src/components/page/Dashboard.vue
@@ -1,105 +1,84 @@
 <template>
     <div>
-        <el-row :gutter="20">
-            <el-col :span="8">
-                <el-card shadow="hover" class="mgb20" style="height:252px;">
-                    <div class="user-info">
-                        <img src="../../assets/img/img.jpg" class="user-avator" alt="">
-                        <div class="user-info-cont">
-                            <div class="user-info-name">{{name}}</div>
-                            <div>{{role}}</div>
+        <!--轮播图-->
+        <el-row style="padding: 10px 50px 10px 50px">
+            <el-row type="flex" class="row-bg" justify="center">
+                <el-col :span="24" align="center">
+                    <el-carousel :interval="4000" type="card" height="400px">
+                        <el-carousel-item v-for="item in itemList">
+                            <h3>{{ item.imgUrl }}</h3>
+                        </el-carousel-item>
+                    </el-carousel>
+                </el-col>
+            </el-row>
+        </el-row>
+
+        <!--Java类展示-->
+        <div class="type">
+            java
+        </div>
+        <el-row style="padding: 10px 80px 10px 50px" >
+            <el-row>
+                <el-col :span="9" >
+                    <img src="../../assets/img/login-bg.jpg" class="first_pic" alt="">
+                    <div class="type">
+                        项目一
+                    </div>
+                </el-col>
+
+                <el-row :gutter="5" type="flex" class="row-bg" justify="center">
+                    <el-col :span="12">
+                        <img src="../../assets/img/login-bg.jpg" class="other_pic" alt="">
+                        <div class="type">
+                            项目一
                         </div>
-                    </div>
-                    <div class="user-info-list">上次登录时间：<span>2018-01-01</span></div>
-                    <div class="user-info-list">上次登录地点：<span>东莞</span></div>
-                </el-card>
-                <el-card shadow="hover" style="height:252px;">
-                    <div slot="header" class="clearfix">
-                        <span>语言详情</span>
-                    </div>
-                    Vue
-                    <el-progress :percentage="71.3" color="#42b983"></el-progress>
-                    JavaScript
-                    <el-progress :percentage="24.1" color="#f1e05a"></el-progress>
-                    CSS
-                    <el-progress :percentage="3.7"></el-progress>
-                    HTML
-                    <el-progress :percentage="0.9" color="#f56c6c"></el-progress>
-                </el-card>
-            </el-col>
-            <el-col :span="16">
-                <el-row :gutter="20" class="mgb20">
-                    <el-col :span="8">
-                        <el-card shadow="hover" :body-style="{padding: '0px'}">
-                            <div class="grid-content grid-con-1">
-                                <i class="el-icon-lx-people grid-con-icon"></i>
-                                <div class="grid-cont-right">
-                                    <div class="grid-num">1234</div>
-                                    <div>用户访问量</div>
-                                </div>
-                            </div>
-                        </el-card>
                     </el-col>
-                    <el-col :span="8">
-                        <el-card shadow="hover" :body-style="{padding: '0px'}">
-                            <div class="grid-content grid-con-2">
-                                <i class="el-icon-lx-notice grid-con-icon"></i>
-                                <div class="grid-cont-right">
-                                    <div class="grid-num">321</div>
-                                    <div>系统消息</div>
-                                </div>
-                            </div>
-                        </el-card>
+                    <el-col :span="12">
+                        <img src="../../assets/img/login-bg.jpg" class="other_pic" alt="">
+                        <div class="type">
+                            项目一
+                        </div>
                     </el-col>
-                    <el-col :span="8">
-                        <el-card shadow="hover" :body-style="{padding: '0px'}">
-                            <div class="grid-content grid-con-3">
-                                <i class="el-icon-lx-goods grid-con-icon"></i>
-                                <div class="grid-cont-right">
-                                    <div class="grid-num">5000</div>
-                                    <div>数量</div>
-                                </div>
-                            </div>
-                        </el-card>
+                    <el-col :span="12">
+                        <img src="../../assets/img/login-bg.jpg" class="other_pic" alt="">
+                        <div class="type">
+                            项目一
+                        </div>
+                    </el-col>
+                    <el-col :span="12">
+                        <img src="../../assets/img/login-bg.jpg" class="other_pic" alt="">
+                        <div class="type">
+                            项目一
+                        </div>
                     </el-col>
                 </el-row>
-                <el-card shadow="hover" style="height:403px;">
-                    <div slot="header" class="clearfix">
-                        <span>待办事项</span>
-                        <el-button style="float: right; padding: 3px 0" type="text">添加</el-button>
-                    </div>
-                    <el-table :data="todoList" :show-header="false" height="304" style="width: 100%;font-size:14px;">
-                        <el-table-column width="40">
-                            <template slot-scope="scope">
-                                <el-checkbox v-model="scope.row.status"></el-checkbox>
-                            </template>
-                        </el-table-column>
-                        <el-table-column>
-                            <template slot-scope="scope">
-                                <div class="todo-item" :class="{'todo-item-del': scope.row.status}">{{scope.row.title}}</div>
-                            </template>
-                        </el-table-column>
-                        <el-table-column width="60">
-                            <template slot-scope="scope">
-                                <i class="el-icon-edit"></i>
-                                <i class="el-icon-delete"></i>
-                            </template>
-                        </el-table-column>
-                    </el-table>
-                </el-card>
-            </el-col>
-        </el-row>
-        <el-row :gutter="20">
-            <el-col :span="12">
-                <el-card shadow="hover">
-                    <schart ref="bar" class="schart" canvasId="bar" :data="data" type="bar" :options="options"></schart>
-                </el-card>
-            </el-col>
-            <el-col :span="12">
-                <el-card shadow="hover">
-                    <schart ref="line" class="schart" canvasId="line" :data="data" type="line" :options="options2"></schart>
-                </el-card>
-            </el-col>
+                <el-row :gutter="5" type="flex" class="row-bg" justify="center">
+                    <el-col :span="12">
+                        <img src="../../assets/img/login-bg.jpg" class="other_pic" alt="">
+                        <div class="type">
+                            项目一
+                        </div>
+                    </el-col>
+                    <el-col :span="12">
+                        <img src="../../assets/img/login-bg.jpg" class="other_pic" alt="">
+                        <div class="type">
+                            项目一
+                        </div>
+                    </el-col>
+                    <el-col :span="12">
+                        <img src="../../assets/img/login-bg.jpg" class="other_pic" alt="">
+                        <div class="type">
+                            项目一
+                        </div>
+                    </el-col>
+                    <el-col :span="12">
+                        <img src="../../assets/img/login-bg.jpg" class="other_pic" alt="">
+                        <div class="type">
+                            项目一
+                        </div>
+                    </el-col>
+                </el-row>
+            </el-row>
         </el-row>
     </div>
 </template>
@@ -107,11 +86,18 @@
 <script>
     import Schart from 'vue-schart';
     import bus from '../common/bus';
+    import pic1 from "../../assets/img/img.jpg";
     export default {
         name: 'dashboard',
         data() {
             return {
                 name: localStorage.getItem('ms_username'),
+                itemList:[
+                    {imgUrl: pic1},
+                    {imgUrl: pic1},
+                    {imgUrl: pic1},
+                    {imgUrl: pic1}
+                ],
                 todoList: [{
                         title: '今天要修复100个bug',
                         status: false,
@@ -294,12 +280,15 @@
         margin-bottom: 20px;
     }
 
-    .user-avator {
-        width: 120px;
-        height: 120px;
-        border-radius: 50%;
+    .first_pic {
+        width: 350px;
+        height: 250px;
     }
 
+    .other_pic {
+        width: 200px;
+        height: 150px;
+    }
     .user-info-cont {
         padding-left: 50px;
         flex: 1;
@@ -340,4 +329,20 @@
         height: 300px;
     }
 
+
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 14px;
+        opacity: 0.75;
+        line-height: 200px;
+        margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
 </style>
